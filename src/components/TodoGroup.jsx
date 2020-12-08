5740572af31845720e952786e012005458d4c3a3
Ernest Cheung <ernestcheung@trustmechain.com>
2020-12-08 16:47:10 +0800

    [TodoGenerator][Ernest] Feat: added draft Todo submit in TodoGenerator

--- src/components/TodoGroup.jsx
+++ src/components/TodoGroup.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
+import {v4 as uuidv4} from 'uuid';
 
 export default class TodoList extends Component {
+
+    constructor(props) {
+        super(props);
+    };
+
+    initArraySize = (number) => {
+        return Array.from(Array(number).keys());
+    };
+
     render() {
+        const size = this.props.size;
+        const initArraySize = this.initArraySize(size);
+        const todoItemArr = initArraySize.map(() => (
+            <TodoItem key={uuidv4()}/>
+        ));
+
         return (
             <div>
-                <TodoItem />
+                {todoItemArr}
             </div>
         )
     }
