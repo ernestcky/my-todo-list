1de7d8c32c343c480ef03ca2e76d10034174132f
Ernest Cheung <ernestcheung@trustmechain.com>
2020-12-08 16:22:25 +0800

    [Todo][Ernest] feat: added basic component

---
+++ src/components/TodoGenerator.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TodoGenerator extends Component {
+    render() {
+        return (
+            <div>
+                Generator
+            </div>
+        )
+    }
+}
