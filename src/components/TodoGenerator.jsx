f63dbbdf83dbde17d3e5270d7352eb1bb5a39231
Ernest Cheung <ernestcheung@trustmechain.com>
2020-12-09 15:16:45 +0800

    added API url

--- src/components/TodoGenerator.jsx
+++ src/components/TodoGenerator.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { addNewTodo } from "../apis/todos";
+import { submitTodo } from './../actions';
 
 export default class TodoGenerator extends Component {
     constructor(props) {
@@ -17,15 +19,16 @@ export default class TodoGenerator extends Component {
     }
     // todo: clear input
     submitTodo = () => {
-        const todo =  {
-            id: uuidv4(),
-            text: this.state.todoText,
-            done: false
-        };
+        addNewTodo(this.state.todoText).then(response => this.props.submitTodo(response.data));
+        // const todo =  {
+        //     id: uuidv4(),
+        //     text: this.state.todoText,
+        //     done: false
+        // };
         this.setState({
             todoText: ""
         })
-        this.props.submitTodo(todo);
+        // this.props.submitTodo(todo);
     }
 
     render() {
