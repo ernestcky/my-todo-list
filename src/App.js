5ac63dd8350a9c7656bb962c5f5cd8a08dda2bc3
Ernest Cheung <ernestcheung@trustmechain.com>
2020-12-09 11:41:27 +0800

    [DoneList][Ernest] Feat: added DoneList Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
 import TodoList from './components/TodoList';
+import { BrowserRouter, Link, NavLink, Redirect, Route, Switch } from "react-router-dom";
+import DoneListContainer from './containers/DoneListContainer'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <TodoList />
+        <h3>Todo List</h3>
       </header>
+      <BrowserRouter>
+        <ul>
+          <li>
+            <NavLink to="/">nav link</NavLink>
+          </li>
+          <li>
+            <Redirect to="/done">go to done page</Redirect>
+          </li>
+          <li>
+            <Link to="/">go to list page</Link>
+          </li>
+          <li>
+            <Link to="/done">go to done page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/done" component={DoneListContainer}/>
+          <Route path="/" component={TodoList}/>
+        </Switch>
+
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/containers/DoneListContainer.jsx
@@ -0,0 +1,11 @@
+import TodoGroup from '../components/TodoGroup';
+import { connect } from "react-redux";
+import DoneList from '../components/DoneList';
+
+const mapStateToProps = (state) => ({
+    doneTodoArray: state.todoArray.filter(todo => todo.done).map(todo => todo.text)
+});
+
+const DoneListContainer = connect(mapStateToProps)(DoneList);
+
+export default DoneListContainer;
